6742b84144026015bec40f39fbb6fa065bf34445
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2020-10-25 23:53:23 +0100

    Fixed #31
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- monolith/views/auth.py
+++ monolith/views/auth.py
@@ -18,7 +18,8 @@ def login():
             login_user(user)
             q = db.session.query(Role).filter(Role.id == user.role_id)
             role = q.first()
-            session["ROLE"] = role.value
+            if role is not None:
+                session["ROLE"] = role.value
             return redirect("/")
         else:
             return render_template("login.html", form=form, message="User not exist")
