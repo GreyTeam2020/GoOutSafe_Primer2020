1e6da213d39a002dcb357c5503124599f3d0a0a1
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2020-10-26 12:09:49 +0100

    Fixed test mock and start first version
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- monolith/tests/conftest.py
+++ monolith/tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
 import sys, os
 from fixtures.client import client
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
--- monolith/tests/fixtures/client.py
+++ monolith/tests/fixtures/client.py
@@ -4,10 +4,12 @@ TODO
 import pytest
 from monolith.app import create_app
 
+
 @pytest.fixture
 def client():
     app = create_app()
     app.config["TESTING"] = True
+    app.config["WTF_CSRF_ENABLED"] = False
     ctx = app.app_context()
     ctx.push()
 
