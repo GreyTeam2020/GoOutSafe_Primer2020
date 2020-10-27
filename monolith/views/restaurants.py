c022d635c79e76e927b1afc04ac327779979d2ff
francesco <ffattori@me.com>
2020-10-27 17:34:39 +0100

    removed useless seconds from tables

--- monolith/views/restaurants.py
+++ monolith/views/restaurants.py
@@ -258,4 +258,4 @@ def my_photogallery():
 
 def my_date_formatter(text):
     date_dt2 = datetime.strptime(text, "%Y-%m-%d %H:%M:%S.%f")
-    return date_dt2.strftime("%d/%m/%Y %H:%M:%S")
+    return date_dt2.strftime("%d/%m/%Y %H:%M")
