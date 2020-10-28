81bb1ca4b6dfcc972a5f3b8ad50cd2d8ad8f2cfb
francesco <ffattori@me.com>
2020-10-28 19:41:36 +0100

    new booking form using WTForms (it breaks Booking, waiting for @giacomofrigo)

--- monolith/views/home.py
+++ monolith/views/home.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 from monolith.database import db, Restaurant, Like
 from monolith.auth import current_user
-
+from monolith.forms import ReservationForm
 
 home = Blueprint("home", __name__)
 
@@ -10,4 +10,5 @@ home = Blueprint("home", __name__)
 @home.route("/")
 def index():
     restaurants = db.session.query(Restaurant)
-    return render_template("index.html", restaurants=restaurants)
+    form = ReservationForm()
+    return render_template("index.html", restaurants=restaurants, form=form)
