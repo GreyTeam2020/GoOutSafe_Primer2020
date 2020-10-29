ae10c1cd145090a7c161976451e327baf3ed5eae
francesco <ffattori@me.com>
2020-10-29 23:04:54 +0100

    fixed tests

--- monolith/tests/test_user_services.py
+++ monolith/tests/test_user_services.py
@@ -4,7 +4,7 @@ import pytest
 from monolith.database import db, User
 from monolith.forms import UserForm
 from monolith.services.user_service import UserService
-from monolith.tests.utils import get_user_with_email
+from monolith.tests.utils import get_user_with_email, login
 
 
 @pytest.mark.usefixtures("client")
@@ -58,7 +58,7 @@ class Test_UserServices:
         db.session.query(User).filter_by(id=user.id).delete()
         db.session.commit()
 
-    def test_modify_user_role_id(self):
+    def test_modify_user_role_id(self, client):
         """
         With this code is tested the services to perform the user modification
         with service and have the result on db
@@ -84,44 +84,14 @@ class Test_UserServices:
         assert user is not None
         assert user.role_id is 2
 
-        user_modified = UserService.modify_user(user, 3)
+        response = login(client, form.email.data, form.password)
+        assert response.status_code == 200
+        assert "logged_test" in response.data.decode("utf-8")
+
+        formTest = UserForm(obj=user)
+        user_modified = UserService.modify_user(formTest, 3)
         assert user is not None
         assert user.role_id is not 2
-        UserService.delete_user(user_modified.id)
-        user_modified = get_user_with_email(user_modified.email)
-        assert user_modified is None
-
-    def test_modify_user_role_id(self):
-        """
-        With this code is tested the services to perform the user modification
-        with service and have the result on db
-
-        Test flow
-        - Create user
-        - Modify user
-        - check user
-        - delete user to clean the database
-        """
-        form = UserForm()
-        form.data["email"] = "alibaba@alibaba.com"
-        form.data["password"] = "Alibaba"
-        form.firstname = "Vincenzo"
-        form.lastname = "Palazzo"
-        form.password = "Alibaba"
-        form.phone = "12345"
-        form.dateofbirth = "12/12/2020"
-        form.email.data = "alibaba@alibaba.com"
-        user = User()
-        form.populate_obj(user)
-        user = UserService.create_user(user, form.password, 2)
-        assert user is not None
-        assert user.role_id is 2
-
-        user.firstname = "Vincent"
-        user_modified = UserService.modify_user(user)
-        assert user is not None
-        assert user_modified.firstname == "Vincent"
-        assert user.role_id is 2
         UserService.delete_user(user_modified.id)
         user_modified = get_user_with_email(user_modified.email)
         assert user_modified is None
@@ -137,14 +107,14 @@ class Test_UserServices:
         """
 
         form = UserForm()
-        form.data["email"] = "alibaba@alibaba.com"
+        form.data["email"] = "alibaba1@alibaba.com"
         form.data["password"] = "Alibaba"
         form.firstname = "Vincenzo"
         form.lastname = "Palazzo"
         form.password = "Alibaba"
         form.phone = "12345"
         form.dateofbirth = "12/12/2020"
-        form.email.data = "alibaba@alibaba.com"
+        form.email.data = "alibaba1@alibaba.com"
         user = User()
         form.populate_obj(user)
         user = UserService.create_user(user, form.password, 2)
@@ -165,14 +135,14 @@ class Test_UserServices:
         """
 
         form = UserForm()
-        form.data["email"] = "alibaba@alibaba.com"
+        form.data["email"] = "alibaba1@alibaba.com"
         form.data["password"] = "Alibaba"
         form.firstname = "Vincenzo"
         form.lastname = "Palazzo"
         form.password = "Alibaba"
         form.phone = "12345"
         form.dateofbirth = "12/12/2020"
-        form.email.data = "alibaba@alibaba.com"
+        form.email.data = "alibaba1@alibaba.com"
         user = User()
         form.populate_obj(user)
         user = UserService.create_user(user, form.password, 2)
