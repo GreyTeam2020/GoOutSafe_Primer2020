39a82b794cb77a7580823e61881b7d60584c52de
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2020-10-29 15:33:16 +0100

    restore francesco changes
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- monolith/views/home.py
+++ monolith/views/home.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-from monolith.database import db, Restaurant, Like
+from monolith.database import db, Restaurant, Like, Positive
 from monolith.auth import current_user
 from monolith.forms import ReservationForm
 
@@ -20,5 +20,5 @@ def index():
     )
 
     return render_template(
-        "index.html", restaurants=restaurants, n_positive=n_positive, n_healed=n_healed
+        "index.html", restaurants=restaurants, form=form,  n_positive=n_positive, n_healed=n_healed
     )
